f70efa1dfcba13a952b9004977a41fea56f68cea
play-builder <playbuilder47@gmail.com>
2024-10-02 18:59:44 +0900

    feat(dev/net): Define environment variables

---
+++ environments/dev/01-network/variables.tf
@@ -0,0 +1,84 @@
+variable "aws_region" {
+  description = "AWS Region for this environment"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name of the project (e.g., myapp)"
+  type        = string
+}
+
+variable "environment" {
+  description = "Name of the environment (e.g., dev)"
+  type        = string
+}
+
+variable "division" {
+  description = "Organizational or technical division responsible for this infrastructure"
+  type        = string
+  default     = "CloudInfra"
+}
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of AWS Availability Zones to use for subnets (e.g. us-east-1a, us-east-1b, ...)"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+}
+
+variable "database_subnet_cidrs" {
+  description = "Database subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Use single NAT Gateway for all AZs"
+  type        = bool
+  default     = true
+}
+
+variable "one_nat_gateway_per_az" {
+  description = "One NAT Gateway per AZ"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS support in VPC"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Additional tags"
+  type        = map(string)
+  default     = {}
+}
