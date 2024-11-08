652c91c99330063849b9118e1bda443d209e18a6
play-builder <playbuilder47@gmail.com>
2024-11-08 18:23:44 +0900

    build(addons): Set versions

---
+++ modules/addons/external-dns/versions.tf
@@ -0,0 +1,19 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.7.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20"
+    }
+  }
+
+}
