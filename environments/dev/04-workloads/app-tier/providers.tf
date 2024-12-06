7aabf726682e533c60b6cf29fb5e544f867dd9a4
play-builder <playbuilder47@gmail.com>
2024-12-06 15:46:07 +0900

    chore(dev/app): Configure providers

---
+++ environments/dev/04-workloads/app-tier/providers.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region     = var.aws_region
+  retry_mode = "adaptive"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = var.project_name
+      ManagedBy   = "terraform"
+      Layer       = "workloads"
+      Application = "online-boutique"
+    }
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
