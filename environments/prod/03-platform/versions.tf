010c22417671cfaa9d091a8a0a3490f819c61dd9
play-builder <playbuilder47@gmail.com>
2025-01-08 19:27:27 +0900

    build(prod): Initialize Prod Platform layer

---
+++ environments/prod/03-platform/versions.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.22.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0.0"
+    }
+  }
+}
