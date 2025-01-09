4cdc237f146fdcc01b303d12f3943503b59796ee
play-builder <playbuilder47@gmail.com>
2025-01-09 10:15:46 +0900

    build(prod): Initialize Prod App layer

---
+++ environments/prod/04-workloads/app-tier/versions.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.22.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10.0"
+    }
+  }
+}
