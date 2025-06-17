81268962135b00b66e760ca749e1046a41bceff4
play-builder <playbuilder47@gmail.com>
2025-06-17 13:52:32 +0900

    fix: change dynamodb to s3 locking and delete unused terraform kubernetes (dev, prod)

--- terraform/iam-github-oidc/backend.tf
+++ terraform/iam-github-oidc/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "plydevops-infra-tf-dev"
-    key            = "shared/iam-github-oidc/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "plydevops-terraform-state-lock-dev"
-    encrypt        = true
+    bucket       = "plydevops-infra-tf-dev"
+    key          = "shared/iam-github-oidc/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
