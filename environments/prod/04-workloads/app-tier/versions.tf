2e99a24e38f05a90171a366a93935a6afc81d462
play-builder <playbuilder47@gmail.com>
2025-07-13 14:22:32 +0900

    fix: Version Update of Terraform and TF-Kubernetes

--- environments/prod/04-workloads/app-tier/versions.tf
+++ environments/prod/04-workloads/app-tier/versions.tf
@@ -1,6 +1,6 @@
 
 terraform {
-  required_version = ">= 1.11.0"
+  required_version = ">= 1.12.0"
 
   required_providers {
     aws = {
@@ -9,7 +9,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.22.0"
+      version = "~> 2.36.0"
     }
     helm = {
       source  = "hashicorp/helm"
