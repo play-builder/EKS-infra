a36ff6ba04943d17d4667cb354f7779c18347480
play-builder <playbuilder47@gmail.com>
2025-07-15 20:13:51 +0900

    feat: added module versions of aws-LB-Controller

---
+++ modules/addons/aws-load-balancer-controller/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.87.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.17.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.22.0"
+    }
+  }
+}
