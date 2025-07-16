dbcc9f054ca914a70ba2db85794d00947f1682a5
play-builder <playbuilder47@gmail.com>
2025-07-16 17:29:43 +0900

    fix: change oidc_provider parsing logic

--- modules/eks/cluster/main.tf
+++ modules/eks/cluster/main.tf
@@ -1,5 +1,5 @@
 locals {
-  oidc_provider = replace(aws_iam_openid_connect_provider.cluster.arn, "/^(.*provider/)/", "")
+  oidc_provider = replace(aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
 }
 
 data "aws_partition" "current" {}
