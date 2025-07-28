cc2633957b7e75f470f12de6caace127b3cc1707
play-builder <playbuilder47@gmail.com>
2025-07-28 17:29:15 +0900

    feat: add SSM access enabled

--- modules/compute/bastion/main.tf
+++ modules/compute/bastion/main.tf
@@ -52,12 +52,38 @@ resource "aws_security_group" "bastion" {
   )
 }
 
+resource "aws_iam_role" "bastion" {
+  name = "${var.name}-bastion-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_ssm" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.name}-bastion-profile"
+  role = aws_iam_role.bastion.name
+}
+
 resource "aws_instance" "bastion" {
   ami                    = data.aws_ami.amazon_linux_2023.id
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
   subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [aws_security_group.bastion.id]
+  iam_instance_profile   = aws_iam_instance_profile.bastion.name
 
   monitoring = true
 
